f96edd02054bb4c0cea19997df8b60a62d5f4fee
Jasur <mr.jasur09@mail.ru>
2021-03-22 23:21:49 +0500

    table of db modified

--- medlight/control/models.py
+++ medlight/control/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-
+from django.utils import timezone
 class Patients(models.Model):
     full_name = models.CharField('ФИО', max_length=50, default='None')
+    date = models.DateField('Дата рождения', auto_now_add=False)
     phone = models.CharField('Номер телефона', max_length=13, default='None')
-    date = models.DateField
 
     def __str__(self):
         return self.title
@@ -13,10 +13,12 @@ class Patients(models.Model):
         verbose_name_plural = 'Пациенты'
 
 class Records(models.Model):
-    patient_id = models.ForeignKey(Patients, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patients, on_delete=models.CASCADE)
     full_name = models.CharField('ФИО', max_length=50, default='')
+    register_date = models.DateTimeField('Время регистрации', default=timezone.now)
     doctor_type = models.CharField('Тип Врача', max_length=30, default='')
-    payed_status = models.BooleanField('Оплатил или нет?')
+    payment_status = models.BooleanField('Оплатил или нет?')
+    used_services = models.CharField('Использованные услуги', max_length=50, default='')
     total_sum = models.CharField('Итого',max_length=50,default='')
     #files = models.FilePathField(path="/home/documents")
 
